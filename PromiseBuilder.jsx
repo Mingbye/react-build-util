8ea4f06baa40ad9ff3022c2707ab4ffd00019517
e-lwanga <lwangaemmanuel@hotmail.com>
2024-12-07 11:06:16 +0300

    1.0.2; bug fix

--- PromiseBuilder.jsx
+++ PromiseBuilder.jsx
@@ -2,28 +2,6 @@ import { useEffect, useState } from "react";
 
 export default function PromiseBuilder({
   promise,
-  builder = (snapshot) => null,
-}) {
-  const [view, setView] = useState(builder(null));
-
-  useEffect(() => {
-    (async () => {
-      let result = undefined;
-      try {
-        result = await promise;
-      } catch (e) {
-        setView(builder([false, e]));
-        return;
-      }
-      setView(builder([true, result]));
-    })();
-  }, []);
-
-  return view;
-}
-
-export default function PromiseBuilder({
-  promise,
   build = (snapshot) => null,
 }) {
   const [snapshot,setSnapshot]=useState(null);
